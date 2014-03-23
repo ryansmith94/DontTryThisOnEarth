d2c009779f98ae4cc927e0e3686c08cf2ff6a14a
Ryan Smith <0ryansmith1994@gmail.com>
2014-03-23 12:51:30 +0000

    Updates

---
+++ scripts/main.js
@@ -0,0 +1,163 @@
+/*
+@author Ryan Smith <12034191@brookes.ac.uk>
+Stores and manipulates user data.
+*/
+
+
+(function() {
+  var Comment, Suggestion, User;
+
+  User = (function() {
+    /*
+    	Constructs a new user.
+    	@param name {String} user's name on the website.
+    	@param email {String} user's email address.
+    */
+
+    function User(name, email) {
+      this.name = name;
+      this.email = email;
+    }
+
+    return User;
+
+  })();
+
+  /*
+  @author Ryan Smith <12034191@brookes.ac.uk>
+  Stores and manipulates comment data.
+  */
+
+
+  Comment = (function() {
+    /*
+    	Constructs a new comment.
+    	@param text {String} comment itself in text.
+    	@param author {User} user that created the comment.
+    	@param date {Date} when the comment was made.
+    */
+
+    function Comment(text, author, date) {
+      this.text = text;
+      this.author = author;
+      this.date = date;
+    }
+
+    /*
+    	Converts the comment to HTML.
+    	@return {String} string of HTML representing the comment.
+    */
+
+
+    Comment.prototype.toHTML = function() {
+      return "<div class=\"comment\">\n	<h2 class=\"text\">" + this.text + "</h2>\n	<div class=\"author\">Posted by <a>" + user.name + "</a> " + this.date + "</div>\n</div>";
+    };
+
+    return Comment;
+
+  })();
+
+  /*
+  @author Ryan Smith <12034191@brookes.ac.uk>
+  Stores and manipulates suggestion data.
+  */
+
+
+  Suggestion = (function() {
+    /*
+    	Constructs a new suggestion.
+    	@param text {String} suggestion itself in text.
+    	@param score {Number} number votes up subtracted by the number of votes down.
+    	@param comments {Array<Comment>} array of comments made about the suggestion.
+    	@param shares {Number} number of times the suggestion was shared.
+    	@param author {User} who made the suggestion.
+    	@param date {Date} when the suggestion was made.
+    */
+
+    function Suggestion(text, score, comments, shares, author, date) {
+      this.text = text != null ? text : '';
+      this.score = score != null ? score : 0;
+      this.comments = comments != null ? comments : [];
+      this.shares = shares != null ? shares : 0;
+      this.author = author != null ? author : null;
+      this.date = date != null ? date : null;
+    }
+
+    /*
+    	Replaces the text and time of a suggestion
+    	@param text {String suggestion itself in text.
+    	@param date {Date} when the suggestion was modified.
+    	@return {Suggestion} the current suggestion.
+    */
+
+
+    Suggestion.prototype.editSuggestion = function(text, date) {
+      this.text = text;
+      this.date = date;
+      return this;
+    };
+
+    /*
+    	Increase the score by one due to up-vote.
+    	@return {Number} the new score (score + 1).
+    */
+
+
+    Suggestion.prototype.voteUp = function() {
+      return this.score += 1;
+    };
+
+    /*
+    	Decreases the score by one due to down-vote.
+    	@return {Number} the new score (score - 1).
+    */
+
+
+    Suggestion.prototype.voteDown = function() {
+      return this.score -= 1;
+    };
+
+    /*
+    	Increase reply counter by one for a new reply.
+    	@param comment {Comment} the comment to be added.
+    	@return {Array<Comment>} the array of comments about the suggestion.
+    */
+
+
+    Suggestion.prototype.addComment = function(comment) {
+      return this.comments.push(comment);
+    };
+
+    /*
+    	Converts the suggestion to HTML.
+    	@param currentUser {Boolean} is the user the current user.
+    	@return {String} string of HTML representing the suggestion.
+    */
+
+
+    Suggestion.prototype.toHTML = (function() {
+      var author, bin;
+      author = function() {
+        return "<div class=\"author\">Posted by <a>" + this.author + "</a> " + this.time + "</div>";
+      };
+      bin = function() {
+        return "<div class=\"delete\">\n  <div class=\"icon\"></div>Delete\n</div>";
+      };
+      return function(currentUser) {
+        var authorHTML;
+        authorHTML = currentUser ? bin : author;
+        return "<div class=\"suggestion\">\n	<div class=\"votes\">\n		<div class=\"up\"></div>\n		<h2 class=\"score\">" + this.score + "</h2>\n		<div class=\"down\"></div>\n	</div>\n	<div class=\"content\">\n		<h1 class=\"text\">\"" + this.text + "\"</h1>\n		<div class=\"info\">\n			<div class=\"reply\">\n				<div class=\"icon\"></div>" + this.replies + " Replies\n			</div>\n			<div class=\"share\">\n				<div class=\"icon\"></div>" + this.shares + " Shares\n			</div>\n			" + (authorHTML()) + "\n		</div>\n	</div>\n</div>";
+      };
+    })();
+
+    return Suggestion;
+
+  })();
+
+  $('.suggestion').click(function(event) {
+    event.stopPropagation();
+    $('.suggestion.selected').removeClass('selected');
+    return $(this).toggleClass('selected');
+  });
+
+}).call(this);
