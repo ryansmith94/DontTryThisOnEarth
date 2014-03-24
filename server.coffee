ecdd0773b2d281e4ad52bd7c2cbdff3a3465ce99
Ryan Smith <0ryansmith1994@gmail.com>
2014-03-24 15:53:18 +0000

    Now using express to serve files via the server.

--- server.coffee
+++ server.coffee
@@ -1,25 +1,7 @@
-fs = require("fs");
-pages = {};
-
-getPage = (path) ->
-    fs.readFile("dist/#{path}", (err, data) ->
-        if err? then throw err else pages[path] = data;
-    )
-    pages[path] or ''
-
-require('http').createServer((req, res) ->
-    if req.method is 'GET'
-        path = req.url.replace('/', '') or 'index.html'
-        console.log(path)
-        if path isnt 'favicon.ico'
-            res.writeHead(200, {
-                'Content-Type': if (path.indexOf('js') isnt -1) then 'text/javascript' else 'text/html'
-            })
-            res.end(getPage(path))
-    else
-        res.writeHead(400, {'Content-Type': 'text/plain'});
-        res.end('Not found!')
-).listen(8000)
+express = require('express');
+app = express();
+app.use(express.static(__dirname + '/dist'));
+app.listen(8000);
 
 console.log('Server running at 127.0.0.1:8000')
-console.log('Go to 127.0.0.1:8000/index.html and refresh a few times')
+console.log('Go to 127.0.0.1:8000/index.html')
--- server.js
+++ server.js
@@ -1,42 +1,16 @@
 (function() {
-  var fs, getPage, pages;
+  var app, express;
 
-  fs = require("fs");
+  express = require('express');
 
-  pages = {};
+  app = express();
 
-  getPage = function(path) {
-    fs.readFile("dist/" + path, function(err, data) {
-      if (err != null) {
-        throw err;
-      } else {
-        return pages[path] = data;
-      }
-    });
-    return pages[path] || '';
-  };
+  app.use(express["static"](__dirname + '/dist'));
 
-  require('http').createServer(function(req, res) {
-    var path;
-    if (req.method === 'GET') {
-      path = req.url.replace('/', '') || 'index.html';
-      console.log(path);
-      if (path !== 'favicon.ico') {
-        res.writeHead(200, {
-          'Content-Type': path.indexOf('js') !== -1 ? 'text/javascript' : 'text/html'
-        });
-        return res.end(getPage(path));
-      }
-    } else {
-      res.writeHead(400, {
-        'Content-Type': 'text/plain'
-      });
-      return res.end('Not found!');
-    }
-  }).listen(8000);
+  app.listen(8000);
 
   console.log('Server running at 127.0.0.1:8000');
 
-  console.log('Go to 127.0.0.1:8000/index.html and refresh a few times');
+  console.log('Go to 127.0.0.1:8000/index.html');
 
 }).call(this);
