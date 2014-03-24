5c61bc21bf93333d34b8690e0098793aa0a52f99
Ryan Smith <0ryansmith1994@gmail.com>
2014-03-24 11:29:03 +0000

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,10 @@
+/*
+@author Ryan Smith <12034191@brookes.ac.uk>. Sky Sanders <http://stackoverflow.com/users/242897/sky-sanders>
+Adapted from [Stack Overflow](http://stackoverflow.com/questions/3177836/how-to-format-time-since-xxx-e-g-4-minutes-ago-similar-to-stack-exchange-site).
+@param date {Date} the date that something was done.
+*/
+
+
 (function() {
   var Comment, Suggestion, User, main, timeSince;
 
@@ -6,17 +13,17 @@
     seconds = Math.floor((new Date() - date) / 1000);
     interval = Math.floor(seconds / 31536000);
     if (interval > 1) {
-      return "" + interval + " years";
+      return "" + interval + " years ago";
     } else if ((interval = Math.floor(seconds / 2592000)) > 1) {
-      return "" + interval + " months";
+      return "" + interval + " months ago";
     } else if ((interval = Math.floor(seconds / 86400)) > 1) {
-      return "" + interval + " days";
+      return "" + interval + " days ago";
     } else if ((interval = Math.floor(seconds / 3600)) > 1) {
-      return "" + interval + " hours";
+      return "" + interval + " hours ago";
     } else if ((interval = Math.floor(seconds / 60)) > 1) {
-      return "" + interval + " minutes";
+      return "" + interval + " minutes ago";
     } else {
-      return "" + (Math.floor(seconds)) + " seconds";
+      return "" + (Math.floor(seconds)) + " seconds ago";
     }
   };
 
