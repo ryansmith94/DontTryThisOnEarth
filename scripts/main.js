51f2bbacdc1284fe10d2988e5da9cc7c94d7eab3
Ryan Smith <0ryansmith1994@gmail.com>
2014-03-24 12:01:15 +0000

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -170,9 +170,10 @@ Adapted from [Stack Overflow](http://stackoverflow.com/questions/3177836/how-to-
         return "<div class=\"delete\">\n  <div class=\"icon\"></div>Delete\n</div>";
       };
       return function(currentUser, id) {
-        var authorHTML, element;
+        var authorHTML, comments, element;
         authorHTML = currentUser ? bin : user;
         element = $("<div class=\"suggestion\" data-suggestion=\"" + id + "\">\n	<div class=\"votes\">\n		<div class=\"up\"></div>\n		<h2 class=\"score\">" + this.score + "</h2>\n		<div class=\"down\"></div>\n	</div>\n	<div class=\"content\">\n		<h1 class=\"text\">\"" + this.text + "\"</h1>\n		<div class=\"info\">\n			<div class=\"reply\">\n				<div class=\"icon\"></div>" + this.comments.length + " Replies\n			</div>\n			<div class=\"share\">\n				<div class=\"icon\"></div>" + this.shares + " Shares\n				<div class=\"shareDropDown\">\n					<a>Facebook</a>\n					<a>Twitter</a>\n				</div>\n			</div>\n			" + (authorHTML(this.author, this.date)) + "\n		</div>\n	</div>\n</div>");
+        comments = this.comments;
         element.click(function(event) {
           var commentsElement;
           event.stopPropagation();
@@ -180,7 +181,7 @@ Adapted from [Stack Overflow](http://stackoverflow.com/questions/3177836/how-to-
           $(this).addClass('selected');
           commentsElement = $('#comments');
           commentsElement.children('.comment').remove();
-          return this.comments.forEach(function(comment) {
+          return comments.forEach(function(comment) {
             return commentsElement.append(comment.toHTML());
           });
         });
